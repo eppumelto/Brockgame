00461647e0cfe3d3b93135516dc2d5fdc9af4886
eppumelto <eppu.melto@hotmail.fi>
2020-11-06 11:15:26 +0200

    Added movement for player

--- 2dkouluhommeli/Assets/scriptit/Movement.cs
+++ 2dkouluhommeli/Assets/scriptit/Movement.cs
@@ -2,48 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class movement : MonoBehaviour
 {
-    public float speed;
-    public float jumpHeight = 4f;
-    public float gravity = -9.81f;
+   
+    public Rigidbody2D rb;
+    public float speed = 12f;
 
-    public CharacterController controller;
-    public Transform groundCheck;
-    public float groundDistace = 0.4f;
-    public LayerMask groundMask;
-
-    Vector3 velocity;
-    bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistace, groundMask);
+        move();
+    }
 
-        if (isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-
+    public void move()
+    {
         float x = Input.GetAxis("Horizontal");
-
-        Vector3 move = transform.right * x;
-        controller.Move(move * speed * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f* gravity);
-        }
-
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
+        float moveby = x * speed;
+        rb.velocity = new Vector2(moveby, rb.velocity.y);
     }
 }
