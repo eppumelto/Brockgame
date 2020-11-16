59362766000cbd176b639c4bb1622e3b4f387949
eppumelto <eppu.melto@hotmail.fi>
2020-11-16 12:34:14 +0200

    added death and death menu

--- 2dkouluhommeli/Assets/scriptit/death.cs
+++ 2dkouluhommeli/Assets/scriptit/death.cs
@@ -1,13 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class death : MonoBehaviour
 {
+    public GameObject deathpanel;
+    public GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
         gameObject.tag = "Player";
+        deathpanel.SetActive(false);
     }
 
     // Update is called once per frame
@@ -16,12 +22,14 @@ public class death : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter(Collider other)
+    public  void OnTriggerEnter2D(Collider2D other)
     {
         
         if (other.tag == "Void")
         {
-            Debug.Log("Kuolit");
+            deathpanel.SetActive(true);
+
+            Destroy(player);
         }
     }
 }
